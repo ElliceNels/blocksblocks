ebc50320033620690aacf4c733488c7c78ad5561
ElliceNels <23371323@studentmail.ul.ie>
2025-04-22 19:29:29 +0100

    [Feat] Add function for downloading keystore

--- src/utils.js
+++ src/utils.js
@@ -10,6 +10,10 @@ if (window.ethereum) {
 function createWallet() {
     const wallet = web3.eth.accounts.create();
     const password = document.getElementById("wallet-password").value;
+    if (password.length < 0 || password == "") {
+        alert("No password provided. Please enter a password.");
+        return;
+    }
     const keystore = web3.eth.accounts.encrypt(wallet.privateKey, password);
     
     document.getElementById("wallet-address").innerText = wallet.address;
@@ -17,4 +21,32 @@ function createWallet() {
     document.getElementById("key-store").innerText = JSON.stringify(keystore);
 }
 
+async function saveWallet() {
+    const keystore = document.getElementById("key-store").innerText;
+    const address = document.getElementById("wallet-address").innerText
+    if (keystore == "") {
+        alert("Please create a wallet first.");
+        return;
+    }
+
+    // Keystore to JSON, JSON to Blob
+    const blob = new Blob([keystore], { type: "application/json" });
+    
+    // Blob to URL
+    const url = URL.createObjectURL(blob);
+
+    // Create a fake link and auto-download
+    const link_element = document.createElement("a");
+    link_element.href = url;
+    link_element.download = `keystore--${address}.json`;
+
+    // Activate the link (download file)
+    link_element.click();
+
+    // Tidy up the stuff
+    URL.revokeObjectURL(url);
+    link_element.remove();
+}
+
 document.getElementById("create-wallet-button").addEventListener("click", createWallet);
+document.getElementById("save-keystore-button").addEventListener("click", saveWallet);
