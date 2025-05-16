d831bad12c0c4ea72aae8f1ba80f6eb3347fa62c
ElliceNels <23371323@studentmail.ul.ie>
2025-05-16 15:56:24 +0100

    [Feat] Add MetaMask support

--- src/js_utils/wallet_display.js
+++ src/js_utils/wallet_display.js
@@ -4,16 +4,15 @@ let web3;
 const sepoliaProvider = "https://sepolia.infura.io/v3/d78d2c3316144eb1aaf1f3fb0e6d4d3a";
 web3 = new Web3(sepoliaProvider);
 
-async function unlockWallet(event) {
+async function unlockWithKeystore(event) {
     event.preventDefault();
     const password = document.getElementById("wallet-password").value;
     const fileInput = document.getElementById("key-store-input");
     const file = fileInput.files[0];
     const keystore = file ? await file.text() : "";
 
-
-    if (keystore == "") {
-        alert("Please create a wallet first.");
+    if (keystore === "") {
+        alert("Please select a keystore file.");
         return;
     }
     try {
@@ -22,11 +21,42 @@ async function unlockWallet(event) {
         const balance = await web3.eth.getBalance(walletAddress); // wallet wei balance
 
         // Display wallet address and balance in Sepolia ETH
-        document.getElementById("wallet-details").innerText = `Address: ${walletAddress}\n\nBalance: ${web3.utils.fromWei(balance, 'ether')} SepETH`;
-        document.getElementById("hamilton-details").innerText = `HamiltonToken Balance: ${10} HTK`;
+        await showWalletDetails(balance, walletAddress, web3);
     } catch (error) {
         alert("Error unlocking wallet: " + error.message);
     }
 }
 
-document.getElementById("wallet-form").addEventListener("submit", unlockWallet);
+async function connectWithMetaMask(event) {
+    event.preventDefault();
+    if (!window.ethereum) {
+        alert("MetaMask is not installed.");
+        return;
+    }
+    try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const metaWeb3 = new Web3(window.ethereum);
+        const accounts = await metaWeb3.eth.getAccounts();
+        if (accounts.length === 0) {
+            alert("No MetaMask accounts found.");
+            return;
+        }
+        const walletAddress = accounts[0];
+        const balance = await metaWeb3.eth.getBalance(walletAddress);
+        
+        await showWalletDetails(balance, walletAddress, metaWeb3);
+    } catch (error) {
+        alert("MetaMask error: " + error.message);
+    }
+}
+
+async function showWalletDetails(balance, walletAddress, web3) {
+    let currencyLabel = "SepETH";
+
+    document.getElementById("detail-heading").innerText = "Wallet Details";
+    document.getElementById("wallet-details").innerText = `Address: ${walletAddress}\n\nBalance: ${web3.utils.fromWei(balance, 'ether')} ${currencyLabel}`;
+    document.getElementById("hamilton-details").innerText = `HamiltonToken Balance: ${10} HSP`;
+}
+
+document.getElementById("wallet-form").addEventListener("submit", unlockWithKeystore);
+document.getElementById("meta-mask-form").addEventListener("submit", connectWithMetaMask);
