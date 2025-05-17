8ae9a5e08988523503887ebaa8b4d89861443675
ElliceNels <23371323@studentmail.ul.ie>
2025-05-17 17:52:28 +0100

    [POST CONTRACT DEPLOYMENT] Make balance show token balance

--- src/js_utils/wallet_display.js
+++ src/js_utils/wallet_display.js
@@ -1,8 +1,11 @@
 let web3;
+import { abi } from "./abi.js";
 
 // connect to the Sepolia testnet
 const sepoliaProvider = "https://sepolia.infura.io/v3/d78d2c3316144eb1aaf1f3fb0e6d4d3a";
 web3 = new Web3(sepoliaProvider);
+const contractAddress = "0x7FdCd17404394CCaad8D89EC274fB8a445646cC8";
+const contract = new web3.eth.Contract(abi, contractAddress);
 
 async function getBalance(event) {
     event.preventDefault();
@@ -14,11 +17,12 @@ async function getBalance(event) {
     }
     try {
         const balance = await web3.eth.getBalance(walletAddress); // wallet wei balance
+        const tokBalance = await contract.methods.balanceOf(walletAddress).call();
         let currencyLabel = "SepETH";
 
         document.getElementById("detail-heading").innerText = "Wallet Details";
         document.getElementById("wallet-details").innerText = `Balance: ${web3.utils.fromWei(balance, 'ether')} ${currencyLabel}`;
-        document.getElementById("hamilton-details").innerText = `HamiltonToken Balance: ${10} HSP`;
+        document.getElementById("hamilton-details").innerText = `HamiltonToken Balance: ${tokBalance} HSP`;
     } catch (error) {
         alert("Error unlocking wallet: " + error.message);
     }
